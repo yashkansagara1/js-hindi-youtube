26598fb8757ec9e688d3c5dcf044c015944a74c4
yashkansagara1 <yashkansagara1@gmail.com>
2024-06-27 11:24:22 +0000

    conversionOperation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,4 +1,25 @@
-let scroe = 33;
+let scroe = null;
 
-console.log(typeofscroe);
-console.log(typeof scroe);
+// console.log(typeof scroe);
+// console.log(typeof(scroe));
+
+let valueINNumber =Number(scroe)
+// console.log(typeof valueINNumber);
+// console.log(valueINNumber);
+
+// "33" => 33 Number
+// "33abc" => NaN
+// true => 1 /false => 0
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 => true / 0 => false
+// "" => false
+// "yash" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
