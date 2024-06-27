32ffc973f79b69d332e5c616b199fed137edd34d
yashkansagara1 <yashkansagara1@gmail.com>
2024-06-27 09:16:12 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123444
+let accountEmail = "yash@google.com"
+var accountPassword = "12345"
+accountCity = "rajkot"
+accountState;
+
+// accountId = 2  not Allowed
+
+accountEmail = "hys@google.com"
+accountPassword = "12344555443333"
+accountCity = "junagadh"
+console.table([accountId, accountEmail, accountPassword, accountCity])
