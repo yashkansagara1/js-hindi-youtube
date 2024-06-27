01db601555fc30531751d7ffe25b395212d757ee
yashkansagara1 <yashkansagara1@gmail.com>
2024-06-27 10:00:16 +0000

    datatypes

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,11 +2,11 @@ const accountId = 123444
 let accountEmail = "yash@google.com"
 var accountPassword = "12345"
 accountCity = "rajkot"
-accountState;
+let accountState;
 
 // accountId = 2  not Allowed
 
 accountEmail = "hys@google.com"
 accountPassword = "12344555443333"
 accountCity = "junagadh"
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert( 3 + 3) // we are useing nodejs ,not browser
+
+console.log( 3 
+    +
+     3) //code  readability should be  high
+
+console.log("yash") 
+
+let name = "yash"
+let age = 28
+let isLoggedin = true
+
+
+// Number => 2 to power 53
+// BigInt =
+// String => "text"
+// Boolean =true/false
+// null => standalone value
+// undefined =>
+// Symbol = unique
+
+// Object 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  // object
