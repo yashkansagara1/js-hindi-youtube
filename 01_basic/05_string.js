56489bf65107d334211685668e24e20c50796705
yashkansagara1 <yashkansagara1@gmail.com>
2024-07-01 10:02:56 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,37 @@
+const name = "yash"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('black_drogan_yash_patel')
+
+// console.log(gameName)
+// console.log(gameName[0]) // key value
+
+// console.log(gameName.__proto__)
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('k'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(2, 6)
+// console.log(anotherString);
+
+const newStringOne = "         yash       "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url ="https://yash.com/yash%20patel"
+
+// console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('_'));
