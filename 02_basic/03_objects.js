02004e07472a42d076e0346fe9ec8bcc8e358057
yashkansagara1 <yashkansagara1@gmail.com>
2024-07-02 14:08:59 +0000

    object

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,18 @@
+// singleton
+// Object.create
+
+// object literals
+
+const jsUser = {
+    name: "yash",
+    "full name": "yash kansagara",
+    age: 28,
+    location: "junagadh",
+    email: "yashpatel@google.com",
+    isLoggedIN: false,
+    lastLoginDays: ["monday", "friday"]
+}
+
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+console.log(jsUser["full name"])
