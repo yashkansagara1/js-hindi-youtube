f74b40fa05cc117d4b770013a5c253eea2e746b3
yashkansagara1 <yashkansagara1@gmail.com>
2024-07-03 05:56:36 +0000

    object part-2

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,67 @@
+// const tinderUser =new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "rahul"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "same@gmail.com",
+    fullName: {
+        userFullName: {
+            firstName: "yash",
+            lastName: "patel"
+        }
+    }
+};
+
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 ={
+    1: "a",
+    2: "b"
+}
+const obj2 ={
+    3: "c",
+    4: "d"
+}
+
+// const  obj3 ={
+//     obj1, obj2
+// }
+
+// const obj3 = Object.assign({} , obj1, obj2)
+
+const obj3 ={ ...obj1, ...obj2}
+// console.log(obj3);
+
+const user =[
+    {
+        id : 1,
+        email: "y@google.com"
+    },
+    {
+        id : 1,
+        email: "y@google.com"
+    },
+    {
+        id : 1,
+        email: "y@google.com"
+    },
+    {
+        id : 1,
+        email: "y@google.com"
+    },
+]
+
+user[1].email
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
