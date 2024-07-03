cbbf3e920611105ed94acb83ce0ec798b3a00d9b
yashkansagara1 <yashkansagara1@gmail.com>
2024-07-03 12:19:10 +0000

    arrow

---
+++ 03_basic/03_arrow_function.js
@@ -0,0 +1,17 @@
+const user = {
+    username : "yash",
+    price: 999,
+
+    Welcomemessage: function(){
+        // console.log(`${this.username} , Welcome to website`);
+        // console.log(this);
+    }
+
+}
+
+// user.Welcomemessage()
+// user.username = "sam"
+// user.Welcomemessage()
+
+// console.log(this)
+
