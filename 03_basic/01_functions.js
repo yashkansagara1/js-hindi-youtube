00d75263b10b57d3875f3f1f725ca81c52664bd7
yashkansagara1 <yashkansagara1@gmail.com>
2024-07-03 09:00:52 +0000

    functions

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,37 @@
+// function saymyname(){
+//     console.log("k");
+//     console.log("a");
+//     console.log("n");
+//     console.log("s");
+//     console.log("a");
+//     console.log("g");
+//     console.log("a");
+//     console.log("r");
+//     console.log("a");
+// }
+// saymyname()
+
+// function addTwoNumber(number1 , number2){
+//     console.log(number1 + number2);
+// }
+
+// function addTwoNumber(number1, number2) {
+// //    let result= number1 + number2
+// //    return result 
+// return number1 + number2
+// }
+
+// const result = addTwoNumber(3, 4)
+
+// console.log("result :",result);
+
+function logInUserMassage(username){
+if(username === undefined){
+    console.log("plase enter a username");
+    return
+}
+
+    // return`${username} just logged In`
+}
+// console.log(logInUserMassage("yash"));
+console.log(logInUserMassage());  // ans undifiend
