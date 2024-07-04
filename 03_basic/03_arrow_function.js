91a69f480736d21e038af0c878c0e804a964c323
yashkansagara1 <yashkansagara1@gmail.com>
2024-07-04 06:40:05 +0000

    Arrow

--- 03_basic/03_arrow_function.js
+++ 03_basic/03_arrow_function.js
@@ -1,8 +1,8 @@
 const user = {
-    username : "yash",
+    username: "yash",
     price: 999,
 
-    Welcomemessage: function(){
+    Welcomemessage: function () {
         // console.log(`${this.username} , Welcome to website`);
         // console.log(this);
     }
@@ -15,3 +15,41 @@ const user = {
 
 // console.log(this)
 
+// function chai(){
+// let username = "yash"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "yash"
+//     console.log(this.username);
+// }
+// chai()
+
+
+// Arrow function
+
+// const chai =  () => {      
+//     let username = "yash"
+//     console.log(this);
+// }
+// chai()
+
+
+// const addTwo =(num1, num2)=>{
+//     return num1 + num2
+// }
+
+// console.log(addTwo(3, 4));
+
+// const addTwo =(num1, num2)=>  num1 + num2
+
+// console.log(addTwo(3, 4));
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username : "yash"})
+
+console.log(addTwo(3, 4));
